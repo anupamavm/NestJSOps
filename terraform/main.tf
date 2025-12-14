1034e05d4e954ac8ed1e0684c6c0a6845a7a55e8
Anupama Morapitiya <anupamamorapitiya97@gmail.com>
2025-12-14 23:55:47 +0530

    feat: add Kubernetes deployment, service, namespace, and secret configurations

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,76 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"  # Change if needed
+}
+
+# ECR Repository
+resource "aws_ecr_repository" "simple_backend" {
+  name                 = "simple-backend"
+  image_tag_mutability = "MUTABLE"
+}
+
+# EKS Cluster
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "~> 19.0"
+  cluster_name    = "simple-eks-cluster"
+  cluster_version = "1.29"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    default = {
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+
+      instance_types = ["t3.small"]
+      capacity_type  = "ON_DEMAND"
+    }
+  }
+
+  # Enable IAM roles for service accounts
+  enable_irsa = true
+}
+
+# Basic VPC (for EKS)
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = "simple-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+}
+
+# Output ECR URL and EKS endpoint
+output "ecr_repository_url" {
+  value = aws_ecr_repository.simple_backend.repository_url
+}
+
+output "eks_cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "eks_kubeconfig" {
+  value = module.eks.kubeconfig
+}
+
 # RDS PostgreSQL
 resource "aws_db_subnet_group" "simple_rds" {
   name       = "simple-rds-subnet-group"
